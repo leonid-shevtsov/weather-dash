f7dcef618cca6441d7bb4f3167b6e1f9ca013729
Leonid Shevtsov <leonid@shevtsov.me>
2019-03-17 19:11:04 +0200

    releasing gh-pages

--- src/weather_page/local_storage.cljs
+++
@@ -1,9 +0,0 @@
-(ns weather-page.local-storage)
-
-(defn read-config []
-  (if-let [config-str (.getItem js/localStorage "config")]
-    (js->clj (.parse js/JSON config-str) :keywordize-keys true)
-    nil))
-
-(defn write-config [config]
-  (.setItem js/localStorage "config" (.stringify js/JSON (clj->js config))))
--- test/weather_page/test_runner.cljs
+++
@@ -1,7 +0,0 @@
-(ns weather-page.test-runner
-  (:require [doo.runner :refer-macros [doo-tests]]
-            weather-page.logic-test
-            weather-page.data-fetcher-test))
-
-(doo-tests 'weather-page.logic-test
-           'weather-page.data-fetcher-test)
